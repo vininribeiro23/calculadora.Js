730f4ff0ab20fd1ea172d340f29d05af2e6ca022
vininribeiro23 <vininribeiro@hotmail.com>
2021-03-23 01:16:58 -0300

    Data e Hora

--- scripts/controler/calcControler.js
+++ scripts/controler/calcControler.js
@@ -1,18 +1,33 @@
 class CalcController {
 
     constructor(){
-        
+        this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
-      this._currentDate  ; 
-      this.initialize();
+        this._currentDate  ; 
+        this.initialize();
     }
 
     initialize(){
+        this.setDisplayDateTime();  
 
-        
+        setInterval(() => { 
+          this.setDisplayDateTime();
 
+        }, 1000);
+
+       
+
+    }
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            day: "2-digit",
+            month:"long" ,
+            year: "numeric",
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
     get displayTime(){
